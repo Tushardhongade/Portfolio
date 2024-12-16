4a3e59d88a873922098e101a189d18bed8ea8d51
Tushardhongade <tushardhongade615@gmail.com>
2024-12-16 19:19:01 +0530

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,18 +13,18 @@ const Home = () => {
           <h1 className='text-5xl mb-4 font-semibold '>Tushar Dhongade</h1>
           <p className='text-2xl text-[#8391AD] leading-'>A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.</p>
           <div className='flex gap-6  mt-6'>
-            <a href="https://github.com/Tushardhongade/" ><img className='rounded-[50%]' src="src/Imgs/icons/github.png" height='50px' width='50px' alt="Profile" /></a>
-            <a href="https://www.linkedin.com/in/tushar-dhongade-4ba929312/" ><img className='rounded-[50%]' src="src/Imgs/icons/linkedin.webp" height='50px' width='50px' alt="Profile" /></a>
-            <a href="https://www.youtube.com/channel/UC1yYU_kyaenJRkmvSPXambw" ><img className='rounded-[50%] bg-[#FF0000]' src="src/Imgs/icons/youtube.png" height='50px' width='50px' alt="Profile" /></a>
-            <a href="mailto:tushardhongade615@gmail.com" ><img className='rounded-[50%]' src="src/Imgs/icons/gmail.png" height='50px' width='50px' alt="Profile" /></a>
-            <a href="https://www.instagram.com/tusha_r9__7___3/" ><img className='rounded-[50%]' src="src/Imgs/icons/insta.webp" height='50px' width='50px' alt="Profile" /></a>
+            <a href="https://github.com/Tushardhongade/" ><img className='rounded-[50%]' src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/github.png" height='50px' width='50px' alt="Profile" /></a>
+            <a href="https://www.linkedin.com/in/tushar-dhongade-4ba929312/" ><img className='rounded-[50%]' src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/linkedin.webp" height='50px' width='50px' alt="Profile" /></a>
+            <a href="https://www.youtube.com/channel/UC1yYU_kyaenJRkmvSPXambw" ><img className='rounded-[50%] bg-[#FF0000]' src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/youtube.png" height='50px' width='50px' alt="Profile" /></a>
+            <a href="mailto:tushardhongade615@gmail.com" ><img className='rounded-[50%]' src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/gmail.png" height='50px' width='50px' alt="Profile" /></a>
+            <a href="https://www.instagram.com/tusha_r9__7___3/" ><img className='rounded-[50%]' src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/insta.webp" height='50px' width='50px' alt="Profile" /></a>
           </div>
           <button className='bg-[#00072D] text-white rounded-md px-6 py-2 mt-8 text-xl'>
           ⭐ Star Me On Github
           </button>
         </div>
         <div className='w-[50%] '>
-          <img src="src/Imgs/feeling_proud.svg" height='900px' width='900px' alt="Profile" />
+          <img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/feeling_proud.svg" height='900px' width='900px' alt="Profile" />
         </div>
         
 
@@ -34,7 +34,7 @@ const Home = () => {
       <div className='flex justify-between items-center mt-20  w-full h-[100vh]'>
 
         <div className='w-[50%] '>
-          <img src="src/Imgs/Frontend.png" height='500px' width='500px' alt="Profile" />
+          <img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/Frontend.png" height='500px' width='500px' alt="Profile" />
         </div>
 
 
@@ -42,11 +42,11 @@ const Home = () => {
           <h3 className='text-4xl font-normal ' >Frontend Developer</h3>
           <div>
             <div className='flex gap-4 mt-6'>
-              <img className="rounded-[50%]" src="src/Imgs/icons/html.png" height='70px' width='70px' alt="" />
-              <img src="src/Imgs/icons/css.png" height='70px' width='70px' alt="Profile" />
-              <img src="src/Imgs/icons/js.png" height='70px' width='70px' alt="Profile" />
-              <img src="src/Imgs/icons/bootstrap.png" height='70px' width='70px' alt="Profile" />
-              <img src="src/Imgs/icons/React.png" height='70px' width='70px' alt="Profile" />
+              <img className="rounded-[50%]" src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/html.png" height='70px' width='70px' alt="" />
+              <img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/css.png" height='70px' width='70px' alt="Profile" />
+              <img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/js.png" height='70px' width='70px' alt="Profile" />
+              <img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/bootstrap.png" height='70px' width='70px' alt="Profile" />
+              <img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/icons/React.png" height='70px' width='70px' alt="Profile" />
               
             </div>
           </div>
