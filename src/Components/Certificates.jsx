b52507b6d4b7f5f92877966091c4b7a580736aab
Tushar Dhongade <109501445+Tushardhongade@users.noreply.github.com>
2025-09-25 15:56:22 +0530

    Update Certificates.jsx

--- src/Components/Certificates.jsx
+++ src/Components/Certificates.jsx
@@ -4,16 +4,16 @@ const Certificates = () => {
   return (
     <div className='flex justify-around items-center flex-wrap my-12'>
         <div className=''>
-            <a href="#"><img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/Certificate1.png" alt="" className='h-56 '/></a>
+            <a href="#"><img src="https://github.com/Tushardhongade/Portfolio/blob/main/src/Imgs/Certificate1.png" alt="" className='h-56 '/></a>
         </div>
         <div>
-            <a href="#"><img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/Certificate1.png" alt="" className='h-56 '/></a>
+            <a href="#"><img src="https://github.com/Tushardhongade/Portfolio/blob/main/src/Imgs/Certificate1.png" alt="" className='h-56 '/></a>
         </div>
         <div>
-            <a href="#"><img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/Certificate1.png" alt="" className='h-56 '/></a>
+            <a href="#"><img src="https://github.com/Tushardhongade/Portfolio/blob/main/src/Imgs/Certificate1.png" alt="" className='h-56 '/></a>
         </div>
         <div>
-            <a href="#"><img src="https://github.com/Tushardhongade/Portfolio/tree/main/src/Imgs/Certificate1.png" alt="" className='h-56 ' /></a>
+            <a href="#"><img src="https://github.com/Tushardhongade/Portfolio/blob/main/src/Imgs/Certificate1.png" alt="" className='h-56 ' /></a>
         </div>
     </div>
   )
